7b3ce5d19f6c5b422aab1f4efaa30612681a1e40
Tom Abella <tomabella64@gmail.com>
2025-05-26 11:07:57 +0800

    Update sidebar.blade.php

--- resources/views/livewire/templates/sidebar.blade.php
+++ resources/views/livewire/templates/sidebar.blade.php
@@ -133,15 +133,14 @@
                 </div>
                 @endcan
 
-                <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <!-- TODO::REPORTS -->
+                <!-- <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
                     <span class="menu-link menu-center" title="Reports" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
                         <span class="menu-icon me-0">
-                            <!--begin::Svg Icon | path: assets/media/icons/duotune/files/fil003.svg-->
                             <span class="svg-icon svg-icon-muted svg-icon-2hx"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                     <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22Z" fill="black" />
                                     <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="black" />
                                 </svg></span>
-                            <!--end::Svg Icon-->
                         </span>
                     </span>
 
@@ -170,7 +169,7 @@
                             </a>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
                 @canany(
                 [
